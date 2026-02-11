53c2bbca65f56375d5b5b0620f1380b6db7c0571
ptyysnik <romariotkachukko@gmail.com>
2026-02-12 02:21:22 +0700

    Create a LessonGroupPart.cs file and a time class

--- NATKschedule/Models/LessonGroupPart.cs
+++ NATKschedule/Models/LessonGroupPart.cs
@@ -1,6 +1,9 @@
 ﻿namespace NATKschedule.Models
 {
-    public class LessonGroupPart
+    public enum LessonGroupPart
     {
+        FULL = 0,
+        SUB1 = 1,
+        SUB2 = 2 
     }
 }
--- NATKschedule/Models/LessonTime.cs
+++ NATKschedule/Models/LessonTime.cs
@@ -1,6 +1,23 @@
-﻿namespace NATKschedule.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace NATKschedule.Models
 {
+    [Table("lesson_time")]
+
     public class LessonTime
     {
+        [Key]
+        [Column("lesson_time_id")]
+        public int LessonTimeId { get; set; }
+        [Column("lesson_number")]
+        public int LessonNumber { get; set; }
+        [Column("time_start")]
+        public TimeOnly TimeStart { get; set; }
+        [Column("time_end")]
+        public TimeOnly TimeEnd { get; set; }
+
     }
+        
 }
