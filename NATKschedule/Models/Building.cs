33374b0aacc7bb3e28f08bdb7a9797bd056b12b8
ptyysnik <romariotkachukko@gmail.com>
2026-02-12 02:14:50 +0700

    Create the Building.cs and Classroom.cs files. So far, we've just added columns.

--- NATKschedule/Models/Building.cs
+++ NATKschedule/Models/Building.cs
@@ -1,6 +1,20 @@
-﻿namespace NATKschedule.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NATKschedule.Models
 {
+    [Table("building")]
     public class Building
     {
+        [Key]
+        [Column("building_id")]
+        public int BuildingId { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+        [Column("address")]
+        [Required]
+        public string Address { get; set; } = null!;
+        public List<Classroom> Classrooms { get; set; } = new();
     }
 }
