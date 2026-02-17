2d2778e023c643d044098c5369b5b229ba301307
ptyysnik <romariotkachukko@gmail.com>
2026-02-17 23:35:43 +0700

    Implementation of the business logic layer. Initially, we prepared a DTO

--- NATKschedule/Program.cs
+++ NATKschedule/Program.cs
@@ -1,6 +1,23 @@
+using NATKschedule.Data;
+using NATKschedule.Middlewares;
+using NATKschedule.Services;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotNetEnv.Env.Load();
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+$"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+$"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+$"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +25,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,6 +38,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
