edfea7a091a64d7d711c7ccaf04cb27c125fbdb5
ptyysnik <romariotkachukko@gmail.com>
2026-02-18 00:00:38 +0700

    We have created a controller. In this project, it is the ScheduleController class

---
+++ NATKschedule/Controllers/ScheduleController.cs
@@ -0,0 +1,19 @@
+﻿using NATKschedule.Data;
+using NATKschedule.Models;
+using NATKschedule.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace NATKschedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+    }
+}
