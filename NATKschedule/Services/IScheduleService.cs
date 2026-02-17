11f8e370db753954eaa3030553e4a2ae59762d9a
ptyysnik <romariotkachukko@gmail.com>
2026-02-17 23:41:29 +0700

    Create file IScheduleService.cs . This file is an interface

---
+++ NATKschedule/Services/IScheduleService.cs
@@ -0,0 +1,10 @@
+﻿using NATKschedule.DTO;
+
+namespace NATKschedule.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime
+startDate, DateTime endDate);
+    }
+}
